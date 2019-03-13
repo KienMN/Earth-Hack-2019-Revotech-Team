e5f07f635543d0d65f192a4612762f70a7c76af9
Kien Mai Ngoc <kienmn97@gmail.com>
2019-03-13 15:52:42 +0700

    Smoothing functions

---
+++ src/smoothing_functions.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pywt
+from statsmodels.robust import mad
+
+def window_smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError("smooth only accepts 1 dimension arrays.")
+
+    if x.size < window_len:
+        raise ValueError("Input vector needs to be bigger than window size.")
+
+    if window_len < 3:
+        return x
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError("Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+
+    s=np.r_[x[window_len-1: 0: -1], x, x[-2: -window_len-1: -1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y[(window_len//2-1):-(window_len//2)]
+
+def wavelet_smooth(x, wavelet="db4", level=1, title=None):
+    # calculate the wavelet coefficients
+    coeff = pywt.wavedec(x, wavelet, mode = "per")
+    # calculate a threshold
+    sigma = mad(coeff[-level])
+
+    uthresh = sigma * np.sqrt(2*np.log(len(x)))
+    coeff[1:] = (pywt.threshold( i, value=uthresh, mode="soft" ) for i in coeff[1:])
+    
+    # reconstruct the signal using the thresholded coefficients
+    y = pywt.waverec(coeff, wavelet, mode = "per")
+    return y
+
+def fourier_smooth(x, frequency):
+    rft = np.fft.rfft(x)
+    if frequency >= rft.shape[0]:
+        raise ValueError("Frequency is not bigger than rft size")
+    rft[frequency:] = 0
+    return np.fft.irfft(rft)
